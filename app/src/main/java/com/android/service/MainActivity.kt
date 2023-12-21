4af158324653da6ab0417c21224f17732c061e48
JYOTI PANDEY <jyotip9352@gmail.com>
2023-12-21 10:50:00 +0530

    Service

---
+++ app/src/main/java/com/android/service/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.android.service
+
+import android.Manifest
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.android.service.ui.theme.ServiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
+        setContent {
+            ServiceTheme {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {
+                    Intent(applicationContext, RunningService::class.java).also {
+                        it.action = RunningService.Actions.START.toString()
+                        startService(it)
+                    }
+                }) {
+                    Text(text = "Start run")
+
+                }
+
+                Button(onClick = {
+                    Intent(applicationContext, RunningService::class.java).also {
+                        it.action = RunningService.Actions.STOP.toString()
+                        startService(it)
+                    }
+                }) {
+                    Text(text = "Stop run")
+                }
+            }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(){
+    ServiceTheme {
+        Greeting("Android")
+    }
+}
